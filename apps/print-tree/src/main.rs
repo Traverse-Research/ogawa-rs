b151c8d64162f00fe640ab53f7d21f1ed8cf9b2c
Max Oomen <64775926+max-traverse@users.noreply.github.com>
2020-12-14 16:10:36 +0100

    Added functionality for passing the filename as an argument in the example applications. (#8)
    * Added functionality for passing the filename as an argument in the example applications.

--- apps/print-tree/src/main.rs
+++ apps/print-tree/src/main.rs
@@ -158,10 +158,15 @@ fn print_object_structure(reader: &mut dyn ArchiveReader, archive: &Archive) -> 
 }
 
 fn main() -> ogawa_rs::Result<()> {
-    let filepath = "test_assets/Eyelashes01.abc";
+    let args = std::env::args().collect::<Vec<String>>();
+    if args.len() < 2 {
+        return Err(ogawa_rs::OgawaError::Other(anyhow::anyhow!(
+            "Missing required filename argument."
+        )));
+    }
 
-    let mut reader = MemMappedReader::new(filepath)?;
-    // let mut reader = FileReader::new(filepath)?;
+    let mut reader = MemMappedReader::new(&args[1])?;
+    // let mut reader = FileReader::new(&args[1])?;
 
     let archive = Archive::new(&mut reader)?;
 
